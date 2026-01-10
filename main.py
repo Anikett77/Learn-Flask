59ee9dc5ccf7662334977912dede60f584efa0a0
Anikett77 <aniketchaturvedi83@gmail.com>
2026-01-10 22:40:46 +0530

    Changing Static Path and Static Folders

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World</p>"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template("index.html")
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 app.run(debug=True)
